b93cdf3d3fbdb0e7b3ba60b968d4fba38bb09041
Ozgeyves <ozge.ozcan@yrnet.com>
2026-07-19 20:38:06 +0300

    auth.py

--- auth.py
+++ auth.py
@@ -1,17 +1,1 @@
-import streamlit as st
 
-from auth import login
-from config import APP_NAME, APP_VERSION
-from depot import render_depot
-from planning import render_planning
-
-st.set_page_config(page_title=APP_NAME, layout="wide")
-
-role = login()
-
-if role == "tedarik":
-    render_planning()
-elif role == "depo":
-    render_depot()
-
-st.sidebar.caption(f"{APP_NAME} · v{APP_VERSION}")
